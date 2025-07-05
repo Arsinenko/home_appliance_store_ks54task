354580919c29c274de37b450d3a245a159365698
Arsinenko <kimp3061@gmail.com>
2025-07-05 13:31:38 +0300

    GoodsRoute

--- internal/routes/goodRoute.go
+++ internal/routes/goodRoute.go
@@ -1,1 +1,138 @@
 package routes
+
+import (
+	"HomeApplianceStore/internal/services"
+	"encoding/json"
+	"errors"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
+)
+
+func CreateProductHandler(service services.GoodsService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var dto services.CreateGoodDto
+		if err := json.NewDecoder(r.Body).Decode(&dto); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		response, err := service.CreateProduct(r.Context(), dto)
+		if err != nil {
+			if errors.Is(err, services.ProductNotFound) {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+func GetProductHandler(service services.GoodsService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(chi.URLParam(r, "id"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		response, err := service.GetProduct(r.Context(), int32(id))
+		if err != nil {
+			if errors.Is(err, services.ProductNotFound) {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func GetProductsHandler(service services.GoodsService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response, err := service.GetGoods(r.Context())
+		if err != nil {
+			if errors.Is(err, services.ProductNotFound) {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func UpdateProductHandler(service services.GoodsService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var dto services.UpdateGoodDto
+		if err := json.NewDecoder(r.Body).Decode(&dto); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		response, err := service.UpdateGoods(r.Context(), dto)
+		if err != nil {
+			if errors.Is(err, services.ProductNotFound) {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func DeleteProductHandler(service services.GoodsService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(chi.URLParam(r, "id"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		err = service.DeleteGood(r.Context(), int32(id))
+		if err != nil {
+			if errors.Is(err, services.ProductNotFound) {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func NewGoodsRouter(service services.GoodsService) http.Handler {
+	r := chi.NewRouter()
+
+	r.Post("/", CreateProductHandler(service))
+	r.Get("/{id}", GetProductHandler(service))
+	r.Get("/", GetProductsHandler(service))
+	r.Put("/", UpdateProductHandler(service))
+	r.Delete("/{id}", DeleteProductHandler(service))
+
+	return r
+}
